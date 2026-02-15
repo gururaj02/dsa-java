ff2dbbff30a3e952f9f0ba5a656b53488bc5c58e
gururaj02 <ngururaj58@gmail.com>
2026-02-15 23:14:01 +0530

    Some Strings concepts added

---
+++ src/com/guru/dsaconcepts/strings/PrettyPrinting.java
@@ -0,0 +1,18 @@
+package com.guru.dsaconcepts.strings;
+
+public class PrettyPrinting {
+
+    public static void main(String[] args) {
+
+        float f = 453.1274f;
+        System.out.printf("Formatted number is %.2f ", f);
+
+        System.out.println(); // for new line
+
+        System.out.printf("PI value %.3f", Math.PI);
+
+        System.out.println(); // for new line
+
+        System.out.printf("Hello my name is %s and I am %s", "Guru", "Cool");
+    }
+}

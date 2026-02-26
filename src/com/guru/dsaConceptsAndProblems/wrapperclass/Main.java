73bd8b5b6c6098f522e232b2a5ca02e993543cac
gururaj02 <ngururaj58@gmail.com>
2026-02-26 23:12:43 +0530

    Wrapper class Integer, Comparing value

---
+++ src/com/guru/dsaConceptsAndProblems/wrapperclass/Main.java
@@ -0,0 +1,16 @@
+package com.guru.dsaConceptsAndProblems.wrapperclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer a = 100;
+        Integer b = 100;
+
+        System.out.println(a == b);
+
+        Integer x = 200;
+        Integer y = 200;
+
+        System.out.println(x == y);
+    }
+}

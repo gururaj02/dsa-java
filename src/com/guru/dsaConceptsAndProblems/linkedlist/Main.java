cbc3e599cc6300e9fd01ee4fc0576b8639d25fd6
gururaj02 <ngururaj58@gmail.com>
2026-03-31 23:04:44 +0530

    Doubly Linked List created
    * Insert first
    * Display and display in reverse

--- src/com/guru/dsaConceptsAndProblems/linkedlist/Main.java
+++ src/com/guru/dsaConceptsAndProblems/linkedlist/Main.java
@@ -4,33 +4,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SinglyLinkedList list = new SinglyLinkedList();
+//        SinglyLinkedList list = new SinglyLinkedList();
+//
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(100, 3);
+//
+//        list.display();
+//        System.out.println("Size : " + list.getSize());
+//
+//        System.out.println(list.deleteFirst());
+//
+//        list.display();
+//        System.out.println("Size : " + list.getSize());
+//
+//        System.out.println(list.deleteLast());
+//
+//        list.display();
+//        System.out.println("Size : " + list.getSize());
+//
+//        System.out.println(list.delete(2));
+//
+//        list.display();
+//        System.out.println("Size : " + list.getSize());
+//
+//        System.out.println(list.find(3));
+
+        DoublyLinkedList list = new DoublyLinkedList();
 
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(8);
         list.insertFirst(17);
-        list.insertLast(99);
-        list.insert(100, 3);
 
         list.display();
-        System.out.println("Size : " + list.getSize());
-
-        System.out.println(list.deleteFirst());
-
-        list.display();
-        System.out.println("Size : " + list.getSize());
-
-        System.out.println(list.deleteLast());
-
-        list.display();
-        System.out.println("Size : " + list.getSize());
-
-        System.out.println(list.delete(2));
-
-        list.display();
-        System.out.println("Size : " + list.getSize());
-
-        System.out.println(list.find(3));
     }
 }

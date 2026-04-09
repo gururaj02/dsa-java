d7f1172f7a5feb81ab4dd16c9c56acf888e2a58d
gururaj02 <ngururaj58@gmail.com>
2026-04-09 18:25:05 +0530

    Deque inbuilt methods added

--- src/com/guru/dsaConceptsAndProblems/queues/InBuiltExample.java
+++ src/com/guru/dsaConceptsAndProblems/queues/InBuiltExample.java
@@ -1,5 +1,7 @@
 package com.guru.dsaConceptsAndProblems.queues;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -7,6 +9,7 @@ public class InBuiltExample {
 
     public static void main(String[] args) {
 
+        // Queue
         Queue<Integer> queue = new LinkedList<>();
 
         queue.add(3);
@@ -18,5 +21,13 @@ public class InBuiltExample {
         System.out.println(queue.peek());
         System.out.println(queue.remove());
         System.out.println(queue.peek());
+
+        // Deque
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        deque.add(3);
+        deque.addLast(78);
+        deque.removeFirst();
+        // etc.,
     }
 }

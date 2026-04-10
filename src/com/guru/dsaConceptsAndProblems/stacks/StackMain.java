e580a66d19d36e4dd5aa6054d99fa7253684a140
gururaj02 <ngururaj58@gmail.com>
2026-04-10 07:35:08 +0530

    Custom Stack implemented
    * push, pop, and peek methods added

---
+++ src/com/guru/dsaConceptsAndProblems/stacks/StackMain.java
@@ -0,0 +1,23 @@
+package com.guru.dsaConceptsAndProblems.stacks;
+
+public class StackMain {
+
+    public static void main(String[] args) throws Exception {
+
+        CustomStack stack = new CustomStack(5);
+
+        System.out.println(stack.push(34));
+        System.out.println(stack.push(45));
+        System.out.println(stack.push(2));
+        System.out.println(stack.push(9));
+        System.out.println(stack.push(18));
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+
+    }
+}

1c32c9f1e81db12c37933dd8c08c57de1ac69bcf
gururaj02 <ngururaj58@gmail.com>
2026-04-13 21:55:40 +0530

    Custom queue implementation
    * insert
    * remove
    * front
    methods added

---
+++ src/com/guru/dsaConceptsAndProblems/queues/QueueMain.java
@@ -0,0 +1,23 @@
+package com.guru.dsaConceptsAndProblems.queues;
+
+public class QueueMain {
+
+    public static void main(String[] args) throws Exception {
+        CustomQueue queue = new CustomQueue(5);
+
+        queue.insert(3);
+        queue.insert(6);
+        queue.insert(5);
+        queue.insert(19);
+        queue.insert(1);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+
+        queue.display();
+
+        System.out.println(queue.front());
+
+    }
+}

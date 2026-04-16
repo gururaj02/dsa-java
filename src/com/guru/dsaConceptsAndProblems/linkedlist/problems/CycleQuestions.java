a91eb86b77606cc76daf02055d56f7cdcdf0aa5b
gururaj02 <ngururaj58@gmail.com>
2026-04-17 00:26:56 +0530

    Happy Number problem

--- src/com/guru/dsaConceptsAndProblems/linkedlist/problems/CycleQuestions.java
+++ src/com/guru/dsaConceptsAndProblems/linkedlist/problems/CycleQuestions.java
@@ -88,6 +88,37 @@ public class CycleQuestions {
     }
 
 
+    // Happy Number
+    // https://leetcode.com/problems/happy-number/description/
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int findSquare(int number) {
+        int ans = 0;
+
+        while(number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number = number / 10;
+        }
+
+        return ans;
+    }
+
+
     class ListNode {
         int val;
         ListNode next;

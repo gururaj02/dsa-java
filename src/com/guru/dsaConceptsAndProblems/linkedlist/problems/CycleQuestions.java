57e058aae01e4427cafbd9d9bbe63b57527abed7
gururaj02 <ngururaj58@gmail.com>
2026-04-16 23:39:29 +0530

    start of the LL cycle problem

--- src/com/guru/dsaConceptsAndProblems/linkedlist/problems/CycleQuestions.java
+++ src/com/guru/dsaConceptsAndProblems/linkedlist/problems/CycleQuestions.java
@@ -47,6 +47,47 @@ public class CycleQuestions {
         return 0;
     }
 
+    // start of the cycle
+    // https://leetcode.com/problems/linked-list-cycle-ii/
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        // Now we have the cycle, find the start
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        // keep moving both forward and they will meet at cycle start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+
+        return s;
+    }
+
+
     class ListNode {
         int val;
         ListNode next;

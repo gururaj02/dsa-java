96cf3969103fdc9af2b110a02cc96f608b1d4cad
gururaj02 <ngururaj58@gmail.com>
2026-04-18 23:36:16 +0530

    Merge sort in LL

---
+++ src/com/guru/dsaConceptsAndProblems/linkedlist/problems/MergeSort.java
@@ -0,0 +1,65 @@
+package com.guru.dsaConceptsAndProblems.linkedlist.problems;
+
+public class MergeSort {
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode mid = getMid(head);
+        ListNode leftHead = head;
+        ListNode rightHead = mid.next;
+
+        mid.next = null;
+
+        ListNode left = sortList(leftHead);
+        ListNode right = sortList(rightHead);
+
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode tail = dummyHead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+                tail = tail.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+                tail = tail.next;
+            }
+        }
+        tail.next = (list1 != null) ? list1 : list2;
+        return dummyHead.next;
+    }
+
+    public ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}

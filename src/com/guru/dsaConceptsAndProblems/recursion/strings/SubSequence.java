9d0cfe8f8ae03a77ce53b482c5c8612c2f2f4ac9
gururaj02 <ngururaj58@gmail.com>
2026-04-21 23:59:28 +0530

    Update SubSequence.java

--- src/com/guru/dsaConceptsAndProblems/recursion/strings/SubSequence.java
+++ src/com/guru/dsaConceptsAndProblems/recursion/strings/SubSequence.java
@@ -9,8 +9,13 @@ public class SubSequence {
         subSeq("", "abc");
 
         System.out.println(subSeqReturn("", "abc"));
+
+        subSeqAscii("", "abc");
+
+        System.out.println(subSeqAsciiReturn("", "abc"));
     }
 
+    // print sub-sequence
     public static void subSeq(String processed, String unprocessed) {
         if (unprocessed.isEmpty()) {
             System.out.println(processed);
@@ -24,6 +29,7 @@ public class SubSequence {
 
     }
 
+    // return sub-sequence in list
     public static ArrayList<String> subSeqReturn(String processed, String unprocessed) {
         if (unprocessed.isEmpty()) {
             ArrayList<String> list = new ArrayList<>();
@@ -38,5 +44,36 @@ public class SubSequence {
 
         left.addAll(right);
         return left;
+    }
+
+    public static void subSeqAscii(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            System.out.println(processed);
+            return;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        subSeqAscii(processed + ch, unprocessed.substring(1));
+        subSeqAscii(processed, unprocessed.substring(1));
+        subSeqAscii(processed + (ch + 0), unprocessed.substring(1));
+    }
+
+    public static ArrayList<String> subSeqAsciiReturn(String processed, String unprocessed) {
+        if (unprocessed.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        ArrayList<String> first = subSeqAsciiReturn(processed + ch, unprocessed.substring(1));
+        ArrayList<String> second = subSeqAsciiReturn(processed, unprocessed.substring(1));
+        ArrayList<String> third = subSeqAsciiReturn(processed + (ch + 0), unprocessed.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
     }
 }

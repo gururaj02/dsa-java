4f7e92044d9d2d72a1382e913367b86e09085751
gururaj02 <ngururaj58@gmail.com>
2026-04-22 21:43:14 +0530

    Iterative program to print subset

---
+++ src/com/guru/dsaConceptsAndProblems/recursion/strings/SubSetArrayUsingIteration.java
@@ -0,0 +1,35 @@
+package com.guru.dsaConceptsAndProblems.recursion.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubSetArrayUsingIteration {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<List<Integer>> ans = subset(arr);
+
+        System.out.println(ans);
+        // -OR-
+        for (List<Integer> list : ans) {
+            System.out.println(list);
+        }
+    }
+
+    public static List<List<Integer>> subset(int[] arr) {
+        List<List<Integer>> outer = new ArrayList<>();
+
+        outer.add(new ArrayList<>());
+
+        for(int num : arr) {
+            int n = outer.size();
+            for(int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+}

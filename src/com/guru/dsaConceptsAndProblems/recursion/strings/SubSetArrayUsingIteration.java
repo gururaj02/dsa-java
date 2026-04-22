50d6b4a721bc50fee67649ccbd23dd2dc9670e2a
gururaj02 <ngururaj58@gmail.com>
2026-04-22 23:18:48 +0530

    subset duplicates

--- src/com/guru/dsaConceptsAndProblems/recursion/strings/SubSetArrayUsingIteration.java
+++ src/com/guru/dsaConceptsAndProblems/recursion/strings/SubSetArrayUsingIteration.java
@@ -1,13 +1,17 @@
 package com.guru.dsaConceptsAndProblems.recursion.strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSetArrayUsingIteration {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
-        List<List<Integer>> ans = subset(arr);
+//        int[] arr = {1, 2, 3};
+//        List<List<Integer>> ans = subset(arr);
+
+        int[] arr = {1, 2, 2};
+        List<List<Integer>> ans = subsetDuplicates(arr);
 
         System.out.println(ans);
         // -OR-
@@ -26,6 +30,31 @@ public class SubSetArrayUsingIteration {
             for(int i = 0; i < n; i++) {
                 List<Integer> internal = new ArrayList<>(outer.get(i));
                 internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+
+
+    public static List<List<Integer>> subsetDuplicates(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < arr.length; i++) {
+            start = 0;
+            // if current and previous element same then start = end + 1
+            if(i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for(int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
                 outer.add(internal);
             }
         }

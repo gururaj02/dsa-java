46a625b32039c788494d7acad784b6c0451d44cf
gururaj02 <ngururaj58@gmail.com>
2026-06-11 23:13:01 +0530

    Dice problem recursion

---
+++ src/com/guru/dsaConceptsAndProblems/recursion/strings/Dice.java
@@ -0,0 +1,19 @@
+package com.guru.dsaConceptsAndProblems.recursion.strings;
+
+public class Dice {
+    public static void main(String[] args) {
+
+        dice("", 4);
+    }
+
+    public static void dice(String processed, int target) {
+        if(target == 0){
+            System.out.println(processed);
+            return;
+        }
+
+        for(int i = 1; i <= 6 && i <= target; i++) {
+            dice(processed + i, target - i);
+        }
+    }
+}

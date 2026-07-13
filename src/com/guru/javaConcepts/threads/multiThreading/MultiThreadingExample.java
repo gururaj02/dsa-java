ddbc904663fafa9bd73c720114fadeeb5c527532
gururaj02 <ngururaj58@gmail.com>
2026-07-13 22:18:15 +0530

    Create MultiThreadingExample.java

---
+++ src/com/guru/javaConcepts/threads/multiThreading/MultiThreadingExample.java
@@ -0,0 +1,28 @@
+package com.guru.javaConcepts.threads.multiThreading;
+
+import java.util.concurrent.*;
+
+public class MultiThreadingExample {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executor =
+                Executors.newFixedThreadPool(2);
+
+        Callable<Integer> task1 = () -> {
+            Thread.sleep(2000);
+            return 10;
+        };
+
+        Callable<Integer> task2 = () -> {
+            Thread.sleep(1000);
+            return 20;
+        };
+
+        Future<Integer> f1 = executor.submit(task1);
+        Future<Integer> f2 = executor.submit(task2);
+
+        System.out.println(f1.get());
+        System.out.println(f2.get());
+
+        executor.shutdown();
+    }
+}
